a1190755706f2dd900569bbcdbaabfe874d990fb
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2015-01-09 16:08:39 +0530

    Added few producthunt web pages in reference, added package.json, started small hello world server

---
+++ index.js
@@ -0,0 +1,47 @@
+var http = require('http'),
+	express = require('express'),
+	app = express();
+
+var path = require('path');
+var express = require('express');
+var logger = require('morgan');
+var favicon = require('serve-favicon');
+var methodOverride = require('method-override');
+var session = require('express-session');
+var errorHandler = require('errorhandler');
+var multer = require('multer');
+var bodyParser = require('body-parser');
+
+
+//var mongoose = require('mongoose');
+//    "mongodb": "^1.4.26",
+//    "mongoose": "^3.8.21",
+
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(logger('dev'));
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(session({resave:true, saveUninitialized:true, secret: 'uwotm8'}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.set('port', (process.env.PORT || 80));
+app.use(express.static(path.join(__dirname , 'public')));
+app.use(errorHandler({dumpExceptions:true, showStack:true}));
+
+
+app.get('/', function(req, res){
+	res.end("Hello World");
+});
+
+http.createServer(app).listen(app.get('port'),function(err){
+	console.log("Started server");
+});
